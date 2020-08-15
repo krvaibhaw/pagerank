06601deddc9d94a22fd23c40a590032df8e1f962
Vaibhaw <kvaibhaw99@gmail.com>
2020-08-15 11:15:00 -0400

    page rank program

--- pagerank.py
+++ pagerank.py
@@ -26,15 +26,21 @@ SAMPLES = 100000
 
 
 def main():
+
     if len(sys.argv) != 2:
         sys.exit("Usage: python pagerank.py corpus")
+
     corpus = crawl(sys.argv[1])
     ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
     print(f"PageRank Results from Sampling (n = {SAMPLES})")
+
     for page in sorted(ranks):
-        print(f"  {page}: {ranks[page]:.4f}")
+    	print(f"  {page}: {ranks[page]:.4f}")
+
     ranks = iterate_pagerank(corpus, DAMPING)
     print(f"PageRank Results from Iteration")
+    
+
     for page in sorted(ranks):
         print(f"  {page}: {ranks[page]:.4f}")
 
